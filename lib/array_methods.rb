0f40adf0acaf47b022c536391ac2b7e4a7fe14ea
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-22 07:20:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,5 +21,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  array.sort[0]
+  counter = 0
+  number = 1 
+  while counter < array.length
+    if array[counter] < number
+      number = array[counter]
+    end
+    counter += 1
+  end
+  number
 end
