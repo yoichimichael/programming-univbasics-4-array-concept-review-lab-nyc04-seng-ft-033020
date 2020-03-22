7b77adfcd6ce11b70cd7275aced2631a0c898b9e
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-22 07:04:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while array[counter] do
-    if array[counter] = value_to_find
+    if array[counter] == value_to_find
       return counter
     end
     counter += 1
