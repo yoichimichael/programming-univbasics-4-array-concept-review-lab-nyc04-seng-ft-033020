bf3c24256027bf8d6970c03222ee3e7cfb86e3a4
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-22 07:04:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,11 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  counter = 0 
+  while array[counter] do
+    if array[counter] = value_to_find
+      return counter
+    end
+    counter += 1
+  end
 end
 
 def find_max_value(array)
